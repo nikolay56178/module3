af27e664a5c702a559f31a6f1db1bc09061298ab
Nikolay56178 <nikolay4156@gmail.com>
2024-01-21 22:21:12 +0300

    's2'

--- 2.2.py
+++ 2.2.py
@@ -1,9 +1,12 @@
 from random import randint
 
-n=5
+n=3
 m=[[randint(0,100) for i in range(n)] for j in range (n)]
 
 print (m)
 
-for i in m:
-	print (max(i))
+c = []
+for l in m:
+    c.append(max(l))
+
+print(max(c))
